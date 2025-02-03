1f77872eb2bf0dae54db2e292a5e5f28890a818e
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2025-02-03 20:58:23 -0300

    Add files via upload

--- js/scrollrevel.js
+++ js/scrollrevel.js
@@ -2,22 +2,22 @@ ScrollReveal({ reset: true });
 
 ScrollReveal().reveal('.TextoPrincipal', 
     {    
-        duration: 1000, reset: false, distance: '100px' , origin: 'left',
+        duration: 700, reset: false, distance: '100px' , origin: 'left',
     }
 );
 ScrollReveal().reveal('#Git', 
     {    
-        delay: 800, reset: false, distance: '100px' , origin: 'left',
+        delay: 600, reset: false, distance: '100px' , origin: 'left',
     }
 );
 ScrollReveal().reveal('#Link', 
     {    
-        delay: 900, reset: false, distance: '100px' , origin: 'left',
+        delay: 700, reset: false, distance: '100px' , origin: 'left',
     }
 );
 ScrollReveal().reveal('#curriculo', 
     {    
-        delay: 1000, reset: false, distance: '100px' , origin: 'left',
+        delay: 900, reset: false, distance: '100px' , origin: 'left',
     }
 );
 ScrollReveal().reveal('#Mouse', 
